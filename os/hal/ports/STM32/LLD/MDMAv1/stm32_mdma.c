1038f62f36e0ee39f31d1889a8911f484b5e1067
Alexandre Bustico <alexandre.bustico@enac.fr>
2022-03-21 23:12:44 +0100

    restore stm32_mdma.c

--- os/hal/ports/STM32/LLD/MDMAv1/stm32_mdma.c
+++ os/hal/ports/STM32/LLD/MDMAv1/stm32_mdma.c
@@ -66,12 +66,13 @@ static struct {
 /*===========================================================================*/
 
 static void mdma_serve_interrupt(const stm32_mdma_channel_t *mdmachp) {
-  uint32_t flags;
+  uint32_t flags, eflags;
 
-  flags = mdmachp->channel->CISR | (mdmachp->channel->CESR << STM32_MDMA_FLAGS_CESR_Pos);
-  mdmachp->channel->CIFCR = flags & STM32_MDMA_FLAGS_CISR_Mask;
+  flags = mdmachp->channel->CISR;
+  eflags = mdmachp->channel->CESR;
+  mdmachp->channel->CIFCR = flags;
   if (mdmachp->func != NULL) {
-    mdmachp->func(mdmachp->param, flags);
+    mdmachp->func(mdmachp->param, flags | (eflags << 16));
   }
 }
 
