c8b62c7e29661cbc433b69efb8656791da9f6159
Alexandre Bustico <alexandre.bustico@enac.fr>
2022-03-21 23:22:33 +0100

    modify CESR/CISR pos and mask according to the official patch

--- os/hal/ports/STM32/LLD/MDMAv1/stm32_mdma.h
+++ os/hal/ports/STM32/LLD/MDMAv1/stm32_mdma.h
@@ -248,8 +248,9 @@
  * @name    ISR flag packing constant
  * @{
  */
-#define STM32_MDMA_FLAGS_CESR_Pos      17U
-#define STM32_MDMA_FLAGS_CISR_Mask     0x1ffff;
+#define STM32_MDMA_FLAGS_CESR_Pos      16U
+#define STM32_MDMA_FLAGS_CISR_Mask     0xffff;
+#define STM32_MDMA_FLAGS_CESR_Mask     0xffff0000;
 /** @} */
 /*===========================================================================*/
 /* Driver pre-compile time settings.                                         */
